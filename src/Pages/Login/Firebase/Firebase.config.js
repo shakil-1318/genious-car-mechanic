d06ca550164bca39381bee80362e0e975169b170
shakil-1318 <shakiljust21@gmail.com>
2021-10-27 23:34:32 +0600

    safety

--- src/Pages/Login/Firebase/Firebase.config.js
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -1,9 +1,11 @@
+console.log(process.env);
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBMd-GUY7ABgeCr2431j4U3CGeREbT0nX8",
-    authDomain: "genius-car-mechanic-a5fe1.firebaseapp.com",
-    projectId: "genius-car-mechanic-a5fe1",
-    storageBucket: "genius-car-mechanic-a5fe1.appspot.com",
-    messagingSenderId: "641189165352",
-    appId: "1:641189165352:web:2a7f156c65d68480d0781d"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 export default firebaseConfig;
